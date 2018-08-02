5a592a22bed29891454b3109390baca7abde3385
chisl <info@chisl.io>
2018-08-02 07:36:01 +0200

    Initial commit

---
+++ SX1212_constants.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""SX1212: Ultra-Low Power Integrated 300-510MHz Transceiver"""
+__author__     = "ChISL"
+__copyright__  = "TBD"
+__credits__    = ["Semtech"]
+__license__    = "TBD"
+__version__    = "Version 0.1"
+__maintainer__ = "https://chisl.io"
+__email__      = "info@chisl.io"
+__status__     = "Test"
+__type__       = ["RF"]
+#
+#   THIS FILE IS AUTOMATICALLY CREATED
+#    D O     N O T     M O D I F Y  !
+#
+class REG:
+	MCParam_1 = 0
+	MCParam_2 = 1
+	MCParam_3 = 2
+	MCParam_4 = 3
+	MCParam_5 = 4
+	MCParam_6 = 5
+	MCParam_7 = 6
+	MCParam_8 = 7
+	MCParam_9 = 8
+	MCParam_10 = 9
+	MCParam_11 = 10
+	MCParam_12 = 11
+	MCParam_13 = 12
+	IRQParam_1 = 12
+	IRQParam_2 = 13
+	IRQParam_3 = 14
+	IRQParam_4 = 15
+	RXParam_1 = 16
+	RXParam_2 = 17
+	RXParam_3 = 18
+	RXParam_4 = 19
+	RXParam_5 = 20
+	RXParam_6 = 21
+	SYNCParam = 22
+	TXParam = 26
+	OSCParam = 27
+	PKTParam_1 = 28
+	PKTParam_2 = 29
+	PKTParam_3 = 30
+	PKTParam_4 = 31
